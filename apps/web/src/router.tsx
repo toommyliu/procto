7a3e727c64941a9706b9a4e522ce9be69f8d4fd0
toommyliu <86510873+toommyliu@users.noreply.github.com>
2026-04-07 11:50:18 -0700

    refactor!: monorepo

--- apps/web/src/router.tsx
+++ apps/web/src/router.tsx
@@ -1,10 +1,12 @@
 import { createRouter as createTanStackRouter } from "@tanstack/react-router"
 import { routeTree } from "./routeTree.gen"
+import { dummy } from "@procto/dummy"
+
+console.log('dummy', dummy)
 
 export function getRouter() {
   const router = createTanStackRouter({
     routeTree,
-
     scrollRestoration: true,
     defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
